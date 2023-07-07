982f442e99cb1dd02aacf33583b9c8bd9d24e608
kurinto <barbosaclint@gmail.com>
2023-07-07 15:52:24 +0800

    added dynamic property source added flyway test

--- src/test/java/com/kurinto/TestContainersTest.java
+++ src/test/java/com/kurinto/TestContainersTest.java
@@ -1,6 +1,9 @@
 package com.kurinto;
 
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -16,9 +19,37 @@ public class TestContainersTest {
                     .withDatabaseName("amigoscode-dao-unit-test")
                     .withUsername("amigoscode")
                     .withPassword("password");
+    @DynamicPropertySource
+    private static void registerDataSourceProperties(DynamicPropertyRegistry registry){
+
+        registry.add(
+                "spring.datasource.url",
+                postgreSQLContainer::getJdbcUrl
+                );
+        registry.add(
+                "spring.datasource.username",
+                postgreSQLContainer::getUsername
+        );
+        registry.add(
+                "spring.datasource.password",
+                postgreSQLContainer::getPassword
+        );
+    }
+
     @Test
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isRunning()).isTrue();
         assertThat(postgreSQLContainer.isCreated()).isTrue();
+    }
+
+    @Test
+    void canApplyDBMigrationsWithFlyway() {
+        Flyway flyway = Flyway.configure().dataSource(
+                postgreSQLContainer.getJdbcUrl(),
+                postgreSQLContainer.getUsername(),
+                postgreSQLContainer.getPassword()
+        ).load();
+        flyway.migrate();
+        System.out.println();
     }
 }
