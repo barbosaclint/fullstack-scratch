49640fdb3add65e0f3857a0cfdec1f21159a31a4
kurinto <barbosaclint@gmail.com>
2023-07-19 10:25:53 +0800

    adding jpa test class

---
+++ src/test/java/com/kurinto/customer/CustomerRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.kurinto.customer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository  underTest;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println(applicationContext.getBeanDefinitionCount());
+    }
+
+    @Test
+    void existsCustomersByEmail() {
+        // Given
+
+        // When
+
+        //Then
+    }
+
+    @Test
+    void existsCustomersById() {
+        // Given
+
+        // When
+
+        //Then
+    }
+}
